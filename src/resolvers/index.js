8bbab94d7e1a15ad34508ee28b3b701d2d4a3a73
Elodie Malet <elodie@enviedefraise.fr>
2020-02-06 13:46:10 +0100

    product

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,12 +1,20 @@
 const products = [
     {
+        id: "1",
         name: 'Livre 1',
+    },
+    {
+        id: "2",
+        name: 'Livre 2',
     },
 ];
 
 const resolvers = {
     Query: {
-        products: ()   => products,
+        products: ()    => products,
+        productById: ( parent, {id}, context, info ) => products.find((product) => {
+            return product.id === id ? product : null
+        }),
     },
 };
 
